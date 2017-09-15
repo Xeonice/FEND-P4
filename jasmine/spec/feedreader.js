09b26c7d3fda3ef10d0a9e88635051833c9aa119
Xeonice <ad546971975@gmail.com>
2017-09-15 09:53:49 +0800

    Bug Fix

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -47,43 +47,43 @@ $(function() {
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
 
-        /* TODO:
-         * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
-         * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
-         */
+    /* TODO:
+     * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
+     * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
+     */
 
-         /* TODO:
-          * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
-          * 再次点击的时候是否隐藏。
-          */
+    /* TODO:
+     * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
+     * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
+     * 再次点击的时候是否隐藏。
+     */
 
     describe('The menu', function () {
         //监视toogleClass部分不理解
-       it('is hidden', function () {
-           expect($('body').hasClass('menu-hidden')).toBeTruthy();
-       });
-       var clickMenuIcon = function () {
-           $('.menu-icon-link').click();
-       };
-       it('click to be viewed', function () {
-           clickMenuIcon();
-           expect($('body').hasClass('menu-hidden')).toBeFalsy();
-           clickMenuIcon();
-           expect($('body').hasClass('menu-hidden')).toBeTruthy();
-           //能够再优化吗？
-       })
+        it('is hidden', function () {
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
+        });
+        var clickMenuIcon = function () {
+            $('.menu-icon-link').click();
+        };
+        it('click to be viewed', function () {
+            clickMenuIcon();
+            expect($('body').hasClass('menu-hidden')).toBeFalsy();
+            clickMenuIcon();
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
+            //能够再优化吗？
+        });
     });
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
 
-        /* TODO:
-         * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
-         * 里面至少有一个 .entry 的元素。
-         *
-         * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
-         * 和异步的 done() 函数。
-         */
+    /* TODO:
+     * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
+     * 里面至少有一个 .entry 的元素。
+     *
+     * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
+     * 和异步的 done() 函数。
+     */
     describe('Initial Entries', function () {
 
         beforeEach(function( done ) {
@@ -97,13 +97,13 @@ $(function() {
         afterEach(function() {
             jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
         });
-    })
+    });
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
 
-        /* TODO:
-         * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
-         * 记住，loadFeed() 函数是异步的。
-         */
+    /* TODO:
+     * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+     * 记住，loadFeed() 函数是异步的。
+     */
     describe('New Feed Selection', function () {
         var oldHtml;
         beforeEach(function (done) {
@@ -115,5 +115,5 @@ $(function() {
         it('can be loaded correctly',function () {
             expect($('.feed').html()).not.toEqual(oldHtml);
         });
-    })
+    });
 }());
